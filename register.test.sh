1bf181c8b3c4bf3a8da98e170885a0fddfb22edb
Brian Wiborg <baccenfutter@c-base.org>
2018-10-20 23:56:30 +0200

    docs for new features

--- register.test.sh
+++
@@ -1,7 +0,0 @@
-curl \
-  -X PUT \
-  -H "Accept: application/json; version=1.0.0" \
-  -H "Content-Type: application/json" \
-  -H "Content-Length: 18" \
-  -d '{"zone":"example"}' \
-  http://localhost:1234/register
